0c8afabd1565cc496f1e71d31c513128f60efb88
Paulo <pc5783346@gmail.com>
2022-06-25 17:37:38 -0300

    fix: the Joi object is now created inside the endpoint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { MongoClient } from "mongodb";
-import { schemaMessages, schemaParticipants } from "./src/validations.js";
+import Joi from "joi";
 import cors from "cors";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
@@ -21,6 +21,9 @@ mongoClient.connect().then(() => {
 });
 
 server.post('/participants', (req, res) => {
+    const schemaParticipants = Joi.object({
+        username: Joi.string().required()
+    });
     const value = schemaParticipants.validate({ username: req.body.name });
     if(value.error){
         res.sendStatus(422);
@@ -62,31 +65,34 @@ server.get('/participants', (_,res) => {
 });
 
 server.post('/messages', (req, res) => {
-    const value = schemaMessages.validate({
-        to: req.body.to,
-        text: req.body.text,
-        type: req.body.type,
-    });
-    if(value.error){
-        res.sendStatus(422);
-    }else{
-        db.collection("participants").find({}).toArray().then((participants) => {
-            const participantsList = [...participants.map(lst => lst.name)];
-            const findUser = participantsList.find(lst => req.headers.user === lst);
-            if(findUser){
-                const message = { from: req.headers.user,...req.body, time: dayjs().format('HH:mm:ss') };
-                db.collection("messages").insertOne(message).then(() =>
-                    res.sendStatus(201)
-                ).catch((e) =>
-                    res.sendStatus(500)
-                );
-            }else{
-                res.sendStatus(422);
-            }
-        }).catch((e) =>
-            res.sendStatus(500)
-        );
-    }
+    db.collection("participants").find({}).toArray().then((participants) => {
+        const participantsList = participants.map(lst => lst.name);
+        const schemaMessages = Joi.object({
+            to: Joi.string().required(),
+            text: Joi.string().required(),
+            type: Joi.string().valid('private_message', 'message').required(),
+            from: Joi.string().valid(...participantsList).required()
+        });
+        const value = schemaMessages.validate({
+            to: req.body.to,
+            text: req.body.text,
+            type: req.body.type,
+            from: req.headers.user
+        });
+        if(!value.error){
+            const message = { from: req.headers.user,...req.body, time: dayjs().format('HH:mm:ss') };
+            db.collection("messages").insertOne(message).then(() =>
+                res.sendStatus(201)
+            ).catch((e) =>
+                res.sendStatus(500)
+            );
+        }else{
+            console.log(value.error);
+            res.sendStatus(422);
+        }
+    }).catch((e) =>
+        res.sendStatus(500)
+    );
 });
 
 server.get('/messages', (req, res) => {
@@ -100,6 +106,22 @@ server.get('/messages', (req, res) => {
             res.status(200).send(messageList.slice(-limit));
         }else{
             res.status(200).send(messageList);
+        }
+    }).catch((e) =>
+        res.sendStatus(500)
+    );
+});
+
+server.post('/status', (req, res) => {
+    const user = req.headers.user;
+    db.collection("participants").findOne({ name: user }).then((participant) => {
+        if(participant){
+            db.collection("participants").updateOne({
+                name: participant.name
+            }, { $set: { lastStatus: Date.now() } });
+            res.sendStatus(200);
+        }else{
+            res.sendStatus(404);
         }
     }).catch((e) =>
         res.sendStatus(500)
